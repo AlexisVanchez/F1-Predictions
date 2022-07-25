7527bbdb5a121abee3dea10468c7dbef86b712c1
AlexisVanchez <85561601+AlexisVanchez@users.noreply.github.com>
2022-07-25 21:31:57 +0300

    results

--- src/Components/Main/Home/Results/Results.js
+++ src/Components/Main/Home/Results/Results.js
@@ -1,72 +1,61 @@
-import {useState} from 'react';
+import classes from  './Results.module.css';
+import './Results.module.css';
+import {useEffect, useMemo, useState} from 'react';
+const API = `http://ergast.com/api/f1/2022/5/results.json`
 
 export default function Results(){
 
-    // const [country, setCountry] = useState([])
+    const [country, setCountry] = useState([]);
+    const [result, setResult] = useState([]);
+    const [fetches, setFetches] = useState([]);
+    let [count, setCount] = useState(0);
 
-    var requestOptions = {
-        method: 'GET',
-        redirect: 'follow',
-      };
-      
-    let country = [];
-    let driverInfo = [];
-    let fetches = [];
-
-    async function calendar(){
-        for(let i = 0; i <= 22; i++){
+    useEffect(() => {
+        async function getResults(){
+            for(let i = 1; i <= 22; i++){
             fetches.push(
-            fetch(`http://ergast.com/api/f1/2022/${i}/results.json`, requestOptions)
-            .then(response => response.text())
-            .then(result => {
-                let event = JSON.parse(result);
-                let events = event.MRData.RaceTable.Races[0]
-                // setCountry(events.Circuit.circuitName) -- крашит браузер
-                country.push(events.Circuit.circuitName)
-                driverInfo.push(events.Results[0])
-                // console.log(country);
-                return events
-            })      
-            .catch(error => console.log('error', error))
-            )
-        }
+            await fetch(`http://ergast.com/api/f1/2022/${i}/results.json`)
+                .then(response => response.json())
+                    .then(data => {
+                        let event = Object.entries(data.MRData.RaceTable.Races[0]);
+                        if(event.length > 0){
+                            setResult(state => [...state,event])
+                        }
+                    })
+                )}
+            }
+            getResults();
     }
+    , [])
 
-    Promise.all(fetches).then(() => {
-        calendar();
-        console.log(fetches)
-    })
-    
     return(
-        <div>
-            <table>
+        <div className={classes.races}>
+            <table className={classes.table}>
                 <thead>
                     <tr>
+                        <th>Stage</th>
                         <th>Circuit</th>
                         <th>Date</th>
                         <th>Winner</th>
                         <th>Team</th>
                         <th>Laps</th>
                         <th>Time</th>
-                        </tr>
-                   
+                    </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                    {driverInfo && driverInfo.Driver.map((fetch, index) => {
-                        return(
-                            <td>{fetch}</td>
-                        )
-                    }
-                    )}
-                        {/* <td>Bahrain</td>
-                        <td>20 Mar 2022</td>
-                        <td>Charles Leclerc</td>
-                        <td>Ferrari</td>
-                        <td>57</td>
-                        <td>1:37:33.584</td> */}
-                    </tr>
-                    
+                        {result.map((event, index) => {
+                            return(
+                                <tr key={index}>
+                                    <td>{index+1}</td>
+                                    <td>{event[3][1]}</td>
+                                    <td>{event[5][1]}</td>
+                                    <td>{event[7][1][0].Driver.familyName}</td>
+                                    <td>{event[7][1][0].Constructor.name}</td>
+                                    <td>{event[7][1][0].laps}</td>
+                                    <td>{event[7][1][0].Time.time}</td>
+                                </tr>
+                            )}
+                        )}
                 </tbody>
             </table>
         </div>
---
+++ src/Components/Main/Home/Results/Stage/Stage.js
@@ -0,0 +1,9 @@
+
+
+export default function Stage(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
